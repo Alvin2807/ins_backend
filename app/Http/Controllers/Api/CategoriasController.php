11a21171ea4a5692c9c7fead59a88334735673a4
alvin.pino <alvin.pino@procuraduria.gob.pa>
2023-08-31 22:54:42 -0500

    Cambios de registro categorias

--- app/Http/Controllers/Api/CategoriasController.php
+++ app/Http/Controllers/Api/CategoriasController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Categoria;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests\Categorias\StoreRequest;
 
 class CategoriasController extends Controller
 {
@@ -17,6 +19,7 @@ class CategoriasController extends Controller
 
         $categorias = Categoria::
         select('id_categoria', 'categoria')
+        ->orderBy('id_categoria','desc')
         ->get();
         return response()->json([
             "ok"   =>true,
@@ -35,9 +38,44 @@ class CategoriasController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreRequest $request)
     {
-        //
+        //Registrar categorías
+        try {
+           DB::beginTransaction();
+           $nombre_categoria = strtoupper($request->input('categoria'));
+           $consulta = Categoria::
+           select('id_categoria','categoria')
+           ->where('categoria', $nombre_categoria)->count();
+           if ($consulta > 0) {
+            return response()->json([
+
+                "existe" =>'Ya existe una categoría '.$nombre_categoria
+            ]);
+           } else {
+            $categorias = new Categoria();
+            $categorias->categoria    = $nombre_categoria;
+            $categorias->usuario_crea = strtoupper($request->input('usuario'));
+            $categorias->save();
+
+            DB::commit();
+            return response()->json([
+                "ok" =>true,
+                "data"=>$categorias,
+                "exitoso"=>'Se guardo satisfactoriamente'
+            ]);
+           }
+
+        } catch (\Exception $th) {
+            DB::rollBack();
+            return response()->json([
+                "ok" =>false,
+                "data" =>$th->getMessage(),
+                "errorRegistro" =>'Hubo un error consulte con el Administrador del sistema'
+            ]);
+        }
+       
+           
     }
 
     /**
--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -11,13 +11,14 @@ class Categoria extends Model
 
     public    $table        = "ins_categorias";
     public    $primarykey   = "id_categoria";
-    protected $fillable     = ['id_categoria', 'categoria'];
+    protected $fillable     = ['id_categoria', 'categoria','usuario_crea','usuario_modifica','fecha_modifica'];
     public    $incrementing = true;
     public    $timestamps   = false;
 
     protected $casts  = [
         'id_categoria' => 'integer',
-        'categoria'    => 'string'
+        'categoria'    => 'string',
+        'usuario_crea' => 'string'
     ];
     
 }
